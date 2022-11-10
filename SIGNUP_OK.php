9c9122d73757e33fc2478fe36a1a0b65f0b4d950
Kim-Greeny <40535867+Kim-Greeny@users.noreply.github.com>
2022-11-10 12:43:14 +0900

    Add files via upload

---
+++ SIGNUP_OK.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>회원 가입</title>
+    <style>
+      body { font-family: sans-serif; font-size: 14px; }
+    </style>
+
+  </head>
+  <body>
+    <center><h1>회원 가입이 완료되었습니다.</h1></center>
+
+    <form>
+        <p><input type="button" onclick="MAIN.php">MAIN PAGE</p>
+        <p><input type="button" onclick="LOGIN.php">LOGIN PAGE</p>
+    </form>
+  </body>
+</html>
