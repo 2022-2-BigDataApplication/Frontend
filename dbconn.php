a99997b7589379413d6ccf66ee82f13219c3af68
uommou <ffffff.990917@gmail.com>
2022-11-14 12:56:19 +0900

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,14 @@
-<?
-    $connect=mysql_connect( "localhost", "root", "apmsetup", "DBname") or  
-        die( "Can't connect to DB server"); 
-
-    mysql_select_db("DBname",$connect);
+<?php
+    $conn=mysql_connect( "localhost", "team07", "team07", "team07");
+    if(!$conn){
+        echo "Database Connection Error";
+    }
+    else{
+        // 커넥션 체크용 나중에 주석처리
+        echo "Database Connection Success";
+    }
+    if(mysqli_connect_errno()){
+        echo "Could not connect : ".mysqli_connect_error();
+        exit();
+    }
 ?>
