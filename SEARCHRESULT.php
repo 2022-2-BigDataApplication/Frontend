bd050469b03c8562dcdee76f6b35005c5ec14c6f
uommou <ffffff.990917@gmail.com>
2022-11-15 16:06:52 +0900

    Update SEARCHRESULT.php

--- SEARCHRESULT.php
+++ SEARCHRESULT.php
@@ -1,23 +1,29 @@
-<?php include('header.php')?>
-
-<?php
-    session_start();
-    $conn = mysqli_connect();
-
-    $search = $_GET['search'];
-?>
-
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/Users/김해인/Desktop/NewJelly/MAIN.css">
-</head>
+        <style>
+            * {margin: 0; padding: 0;}
+            #wrap {
+                width=650px;
+                margin: 0 auto;
+                overflow: hidden;
+                padding-top: 10px;
+            }
 
+            div > article {
+                float: left;
+                margin-left: 10px;
+                margin-bottom: 10px;
+            }
+
+            img {display: block;}
+        </style>
+</head>
 
 <body>
     <br><br><br>
     <center>
-    <form class="search" action="LIST.php" method=POST">
+    <form action="searchResearch.php" method="GET">
         
         <h1>Movie Recommender</h1>
     
@@ -28,35 +34,112 @@
     </center>
 
     <br><br>
+
     <hr>
+
     <br>
 
     <div class="serchresult">
         <h3>Search Result</h3>
         
+        <?php $search_key = $_GET['search_key']; ?>
+
         <!--제목포함-->
         <div class="search_title">
             <p>* 제목 포함</p>
-            <form action="SEARCHRESULT.php" method="get">
+            <form action="research.php" method="get">
                 <?php 
-                    $sql1 = "SELECT movieNm, url FROM movie_metadata WHERE movieNm LIKE '%search_key%'";
-                    
+                    $sql1 = "SELECT posterPath FROM movie_metadata WHERE movieNm LIKE '%$search_key%'";
                 ?>
             </form>
+            <?php
+                 $result= mysqli_query($db, $sql1)
+            
+                 while($row = mysqli_fetch_array($result)) {
+                
+                        $poster = $row["posterPath"];
+                
+                ?>
+                <article><img src=<?$poster?>></article>
+                <?php
+            }
+            ?>
+       ?>
         </div>
 
-        <!--배우/감독포함-->
+        <!--배우포함-->
         <div class="search_name">
-            <p>* 인물명 포함</p>
-            <form action="SEARCHRESULT.php" method="get">
+            <p>* 배우 포함</p>
+            <form action="research.php" method="get">
                 <?php 
-                    $sql1 = "SELECT movieNm, url FROM movie_metadata WHERE movieNm LIKE '%search_key%'";
+                    $sql2 = "SELECT posterPath from movie_metadata
+
+                    where movieId in (select movieId from characters where actorId = (SELECT actorId FROM actor WHERE actorName LIKE '%$search_key%'));";
                     
                 ?>
             </form>
+            <?php
+                 $result= mysqli_query($db, $sql2)
+            
+                 while($row = mysqli_fetch_array($result)) {
+                
+                        $poster = $row["posterPath"];
+                
+                ?>
+                <article><img src=<?$poster?>></article>
+                <?php
+            }
+            ?>
         </div> 
-        <!--장르포함-->
-        <div class="search_genre">
-            <p>* 장르 포함</p>
+        
+        <div class="search_name">
+            <p>* 감독 포함</p>
+            <form action="research.php" method="get">
+                <?php 
+                    $sql3 = "SELECT posterPath from movie_metadata
+
+                    where directorId in (select directorId from director where directorName LIKE '%$search_key%');";
+                    
+                ?>
+            </form>
+            <?php
+                 $result= mysqli_query($db, $sql3)
+            
+                 while($row = mysqli_fetch_array($result)) {
+                
+                        $poster = $row["posterPath"];
+                
+                ?>
+                <article><img src=<?$poster?>></article>
+                <?php
+            }
+            ?>
+        </div> 
+        
+        <!--키워드포함-->
+        <div class="search_keyword">
+            <p>* 키워드 포함</p>
+            <form action="research.php" method="get">
+                <?php 
+                    $sql4 = "SELECT posterPath from movie_metadata
+
+                    where movieId in (select movieId from describes
+                    
+                    where keywordId = (select keywordId from keyword where keywordName LIKE '%$search_key%'));";
+                    
+                ?>
+            </form>
+            <?php
+                 $result= mysqli_query($db, $sql4)
+            
+                 while($row = mysqli_fetch_array($result)) {
+                
+                        $poster = $row["posterPath"];
+                
+                ?>
+                <article><img src=<?$poster?>></article>
+                <?php
+            }
+            ?>
         </div>
 </body>
