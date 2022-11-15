5ca3a2707de291811037c1e5caefe5496deb119d
Kim-Greeny <40535867+Kim-Greeny@users.noreply.github.com>
2022-11-15 16:06:17 +0900

    Update and rename SIGNUP_OK.php to join_ok.php

--- join_ok.php
+++ join_ok.php
@@ -1,15 +1,15 @@
 <!doctype html>
-<html lang="ko">
+<html>
   <head>
-    <meta charset="utf-8">
-    <title>회원 가입</title>
+    <meta charset="UTF-8">
+    <title>Join US!</title>
     <style>
       body { font-family: sans-serif; font-size: 14px; }
     </style>
 
   </head>
   <body>
-    <center><h1>회원 가입이 완료되었습니다.</h1></center>
+    <center><h1>Success!</h1></center>
 
     <form>
         <p><input type="button" onclick="MAIN.php">MAIN PAGE</p>
