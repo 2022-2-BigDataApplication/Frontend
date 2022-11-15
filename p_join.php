69ce388d9d7cb5a3404cbbc1d450e5ddef598333
MSY99 <101084413+MSY99@users.noreply.github.com>
2022-11-16 00:34:32 +0900

    회원가입 기능 완성

--- p_join.php
+++ p_join.php
@@ -1,35 +1,3 @@
-<?php
-    session_start();
-    include('dbconn.php');
-?>
-
-<?php
-    $userId = $_POST['userId'];
-    $password = $_POST['pwd'];
-    $userName = $_POST['userName'];
-    $userIntro = $_POST['userIntro'];
-    $password_confirm = $_POST['pwd_confirm'];
-    if (!is_null($userId)){
-        $id_sql = "SELECT userID from users where userId='$userId'";
-        $id_resource = mysqli_query($connect, $id_sql);
-        while ($id_row = mysqli_fetch_row($id_resource)){
-            $userId_e = $id_row['userId'];
-        }
-        if ($userId == $userId_e){
-            $wu = 1;
-        }
-        elseif ($password != $password_confirm) {
-            $wp = 1;
-        }
-        else {
-            $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
-            $user_add_sql = "INSERT into users (userId, pwd, userName, introduction) VALUES ($userId, $password, $userName, $userIntro)";
-            mysqli_query($connect, $user_add_sql);
-            header('Location: login.php');
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,8 +6,8 @@
 <header>
     <h1>New Jelly</h1>
     <nav>
-      <span><a href="login.php">Login</a></span>
-      <span><a href="join.php">Join</a></span>
+      <span><a href="p_login.php">Login</a></span>
+      <span><a href="p_join.php">Join</a></span>
     </nav>
 </header>
   <body>
@@ -47,23 +15,23 @@
     <h2> Join us! </h2>
     <div>
       <table>
-      <form action="join.php" method = "post">
+      <form action="join.php" method = "POST">
         <tr>
-          <td>E-mail</td>
-          <td><input type="text" name="ID" placeholder="Email Address"></td>
+          <td>ID</td>
+          <td><input type="text" name="userId" placeholder="ID" required></td>
         </tr>
 
         <tr><td></td><td></td><td></td></tr>
 
         <tr>
           <td>Password</td>
-          <td><input type="text" name="PW" placeholder="password"></td>
+          <td><input type="text" name="pwd" placeholder="password" required></td>
           <td></td>
         </tr>
 
         <tr>
           <td>Check PW</td>
-          <td><input type="text" name="PW_check" placeholder="check password"></td>
+          <td><input type="text" name="pwd_confirm" placeholder="check password" required></td>
           <td></td>
         </tr>
 
@@ -73,30 +41,22 @@
 
         <tr>
           <td>NickName</td>
-          <td><input type="text" name="name" placeholder="nickname"></td>
+          <td><input type="text" name="userName" placeholder="nickname" required></td>
           <td></td>
         </tr>
 
         <tr>
           <td>Introduction</td>
-          <td><input type="text" name="intro" placeholder="no more than 300byte"></td>
+          <td><input  type="text" name="userIntro" placeholder="no more than 300byte" required></td>
           <td></td>
         </tr>
       </table>
         <br>
 
-        <p><center><input type="submit" name="submit" size="50" value="Done" style="height:30px; width:50px"/></center></p>
-        <?php
-            if ($wu == 1){
-                echo "<p> Duplicate ID </p>";
-            }
-            if ($wp == 1){
-                echo "<p> Check your PW typing </p>";
-            }
-        ?>
+        <p><input type="submit" name="submit" size="50" value="Done" style="height:30px; width:50px; margin: auto; display: block;"/></p>
+        
       </form>       
     </div>
   </main>
-  <?php mysqli_close($con); ?>
 </body>
 </html>
