9a2c5a1b980b1046f8012794a9d59468b74cacd1
Kim-Greeny <40535867+Kim-Greeny@users.noreply.github.com>
2022-11-16 05:47:10 +0900

    구현 완료

---
+++ p_exploreResult.php
@@ -0,0 +1,249 @@
+<?php
+session_start();
+include('dbconn.php');
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="headerCSS.css">
+    <style>
+        * {margin: 0; padding: 0;}
+        #wrap {
+            width=650px;
+            margin: 0 auto;
+            overflow: hidden;
+            padding-top: 10px;
+        }
+
+        div > article {
+            float: left;
+            margin-left: 10px;
+            margin-bottom: 10px;
+        }
+
+        img {display: block;}
+    </style>
+</head>
+
+<body>
+<header>
+        <h1>New Jelly</h1>
+        <nav>
+        <span><a href="logout.php">Logout</a></span>
+        <span><a href="mypage.html">Mypage</a></span>
+        </nav>
+    </header>
+    <div id="filter">
+        <form action="p_exploreResult.php" method="post">
+            <h2>Filtering Option</h2>
+            <br>
+            
+            RelesedDate:<input type="text" name="start_year" placeholder="start_year">
+                        <input type="text" name="end_year" placeholder="end_year">
+                        <br><br>
+                            
+            Genre : <input type="checkbox" name="genre[]" value="21">All
+                    <input type="checkbox" name="genre[]" value="16">Animation
+                    <input type="checkbox" name="genre[]" value="12">Adventure
+                    <input type="checkbox" name="genre[]" value="10749">Romance
+                    <input type="checkbox" name="genre[]" value="35">Comedy
+                    <input type="checkbox" name="genre[]" value="28">Action
+                    <input type="checkbox" name="genre[]" value="10751">Family
+                    <input type="checkbox" name="genre[]" value="36">History
+                    <input type="checkbox" name="genre[]" value="18">Drama
+                    <input type="checkbox" name="genre[]" value="80">Crime
+                    <input type="checkbox" name="genre[]" value="878">Science Fiction
+                    <input type="checkbox" name="genre[]" value="14">Fantasy
+                    <input type="checkbox" name="genre[]" value="10402">Music
+                    <input type="checkbox" name="genre[]" value="27">Horror
+                    <input type="checkbox" name="genre[]" value="99">Documentary
+                    <input type="checkbox" name="genre[]" value="9648">Mystery
+                    <input type="checkbox" name="genre[]" value="53">Thriller
+                    <input type="checkbox" name="genre[]" value="37">Western
+                    <input type="checkbox" name="genre[]" value="10770">TV Movie
+                    <input type="checkbox" name="genre[]" value="10752">War
+                    <input type="checkbox" name="genre[]" value="10769">Foreign
+                    <br><br>
+
+            Age Limitation:<input type="radio" name="age" value="all">All
+                            <input type="radio" name="age" value="kids">Kids
+                    <br><br>
+
+            Viewing : <select name="view">
+                <option value="0">Recently Released</option>
+                <option value="1">Highly Scored</option>
+                <option value="2">Most Commented</option>
+            </select>   
+            <br><br><br>
+            <center><button>SUBMIT</button></center>
+            <br><br><br>
+        </form>
+        <hr>
+    </div>
+    <br><br><br>
+    <br><br><br>
+    <div>
+        <?php
+
+            $start_year= $_POST['start_year'];
+            $end_year= $_POST['end_year'];
+            $genre= $_POST['genre']; 
+            $age= $_POST['age'];
+            $view= $_POST['view'];
+
+            /* 장르전체 */
+            if($genre[0]='21') {
+                /*성인*/
+                if($age='all') {
+                    /*최신순*/
+                    if($view='0') {
+                        $sql = "SELECT originalTitle, posterPath FROM movie_metadata
+                        WHERE DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        ORDER BY openDt desc;";
+                    } 
+                    /*별점순*/
+                    else if ($view='1') {
+                        $sql = "SELECT m.originalTitle, m.posterPath, count(*) AS `review_num` FROM review as r
+                        INNER JOIN movie_metadata as m on r.movieId = m.movieId
+                        WHERE DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        GROUP BY m.movieId
+                        order by review_num desc;";
+                    } 
+                    /*리뷰순*/
+                    else {
+                        $sql = "SELECT m.originalTitle, m.posterPath, avg(r.rating) AS `rating_avg` FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        GROUP BY m.movieId
+                        
+                        order by rating_avg desc;";
+                    }
+                } 
+                /*키즈*/
+                else {
+                    if($view='0') {
+                        $sql = "SELECT originalTitle, posterPath FROM movie_metadata
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        and adult=0
+                        order by openDt desc;";
+                    } 
+                    /*별점순*/
+                    else if ($view='1') {
+                        $sql = "SELECT m.originalTitle, m.posterPath, count(*) AS `review_num`FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        and adult=0
+                        GROUP BY m.movieId
+                        
+                        order by review_num desc;";
+                    } 
+                    /*리뷰순*/
+                    else {
+                        $sql = "SELECT m.originalTitle, m.posterPath, avg(r.rating) AS `rating_avg` FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        and adult=0
+                        GROUP BY m.movieId
+                        
+                        order by rating_avg desc;";
+                    }
+                }
+            } 
+            /* 장르선택 */
+            else {
+                /*성인*/
+                if($age='all') {
+                    /*최신순*/
+                    if($view='0') {
+                        $sql = "SELECT originalTitle, posterPath FROM movie_metadata
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        and (genreId in ($genre))
+                        order by openDt desc;";
+                    } 
+                    /*별점순*/
+                    else if ($view='1') {
+                        $sql = "SELECT m.originalTitle, m.posterPath, count(*) AS `review_num`FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        
+                        and (genreId in $genre)
+                    
+                        GROUP BY m.movieId
+                        
+                        order by review_num desc;";
+                    } 
+                    /*리뷰순*/
+                    else {
+                        $sql = "SELECT m.originalTitle, m.posterPath, avg(r.rating) AS `rating_avg` FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        
+                        and (genreId in $genre)
+                        
+                        GROUP BY m.movieId
+                        
+                        order by rating_avg desc;";
+                    }
+                } 
+                /*키즈*/
+                else {
+                    /*최신순*/
+                    if($view='0') {
+                        $sql = "SELECT originalTitle, posterPath FROM movie_metadata
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        and (genreId in ($genre))
+                        and adult=0
+                        order by openDt desc;";
+                    } 
+                    /*별점순*/
+                    else if ($view='1') {
+                        $sql = "SELECT m.originalTitle, m.posterPath, count(*) AS `review_num`FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        
+                        and (genreId in $genre)
+                        and adult=0
+                        GROUP BY m.movieId
+                        
+                        order by review_num desc;";
+                    } 
+                    /*리뷰순*/
+                    else {
+                        $sql = "SELECT m.originalTitle, m.posterPath, avg(r.rating) AS `rating_avg` FROM review as r
+                        inner join movie_metadata as m on r.movieId = m.movieId
+                        
+                        where DATE_FORMAT(openDt, '%Y') between CAST($start_year AS CHAR(4)) and CAST($end_year AS CHAR(4))
+                        
+                        and (genreId in $genre)
+                        and adult=0
+                        GROUP BY m.movieId
+                        
+                        order by rating_avg desc;";
+                    } 
+                }
+            }
+            
+            $res = mysqli_query($conn, $sql);
+
+            while($row = mysqli_fetch_row($res)) {
+                
+                $poster = "http://image.tmdb.org/t/p/w185/$row[1]";
+                
+                ?> 
+
+                <article><img src=<?=$poster?>></article>
+
+                <?php
+            };
+       ?>
+    </div>
+    
+</body>
+</html>
---
+++ p_searchResult.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+include('dbconn.php');
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="headerCSS.css">
+        <style>
+            * {margin: 0; padding: 0;}
+            #wrap {
+                width=650px;
+                margin: 0 auto;
+                overflow: hidden;
+                padding-top: 10px;
+            }
+
+            div > article {
+                float: left;
+                margin-left: 10px;
+                margin-bottom: 10px;
+            }
+
+            img {display: block;}
+        </style>
+</head>
+
+<body>
+    <!--상단바-->
+    <header>
+            <h1>New Jelly</h1>
+            <nav>
+            <span><a href="logout.php">Logout</a></span>
+            <span><a href="mypage.html">Mypage</a></span>
+            </nav>
+    </header>
+
+    <br><br>
+    <!--검색창-->
+    <div class="text">
+        <br><br><br>
+        <center>
+        <form action="p_searchResult.php" method="POST">
+            
+            <h2>Movie Recommender</h2>
+            <br>
+        
+            <input type="text" name="search_key" placeholder="Enter What You Like"><br><br>
+            <button>Submit</button>
+
+        </form>
+    </center>
+    </div>
+    <hr>
+
+    <br>
+
+    <div class="result">
+        <h3>Search Result</h3>
+        
+        <?php $search_key = $_POST['search_key']; ?>
+        
+        
+        <!--제목포함-->
+        <div>
+            <p>* 제목 포함</p>
+                <?php 
+                    $sql1 = "SELECT posterPath FROM movie_metadata WHERE originalTitle LIKE '%$search_key%';";
+               
+                 $result_title= mysqli_query($conn, $sql1);
+            
+                 while($row1 = mysqli_fetch_row($result_title)) {
+                
+                        $poster = "http://image.tmdb.org/t/p/w185/$row1[0]";
+                
+                ?>
+                <article><img src=<?=$poster?>></article>
+                <?php
+                }
+            ?>
+        </div>
+        <br><br>
+        <!--배우포함-->
+            
+        <div>
+            <p>* 배우 일치</p>
+            <?php 
+                $sql2 = "SELECT posterPath from movie_metadata
+                    where movieId in (select movieId from characters where actorId = ANY(SELECT actorId FROM actor WHERE actorName LIKE '$search_key'));";
+               
+                 $result_actor= mysqli_query($conn, $sql2);
+            
+                 while($row2 = mysqli_fetch_row($result_actor)) {
+                
+                        $poster = "http://image.tmdb.org/t/p/w185/$row2[0]";
+                
+                ?>
+                <article><img src=<?=$poster?>></article>
+                <?php
+                }
+            ?>
+        </div>
+            <br><br>
+
+            <p>* 감독 일치</p>
+            <div>
+                <?php 
+                    $sql3 = "SELECT posterPath from movie_metadata
+                    where directorId in (select directorId from director where directorName LIKE '$search_key');";
+                    
+                ?>
+            
+            <?php
+                 $result_director= mysqli_query($conn, $sql3);
+            
+                 while($row3 = mysqli_fetch_row($result_director)) {
+                
+                        $poster = "http://image.tmdb.org/t/p/w185/$row3[0]";
+                
+                ?>
+                <article><img src=<?=$poster?>></article>
+                <?php
+                }
+            ?>
+            </div>
+        
+        <!--키워드포함-->
+            <br><br>    
+            <div>
+            <p>* 키워드 포함</p>
+                <?php 
+                    $sql4 = "SELECT posterPath from movie_metadata
+                    where movieId in (select movieId from describes
+                    where keywordId = any(select keywordId from keyword where keywordName LIKE '%$search_key%'));";
+                    
+                ?>
+            <?php
+               $result_key= mysqli_query($conn, $sql4);
+            
+               while($row4 = mysqli_fetch_row($result_key)) {
+              
+                      $poster = "http://image.tmdb.org/t/p/w185/$row4[0]";
+              
+              ?>
+              <article><img src=<?=$poster?>></article>
+              <?php
+              }
+          ?> </div>
+    </div>
+</body>
