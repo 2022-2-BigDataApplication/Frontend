0af9d4459476db93a1ee4099d704bd2345c5ba04
MSY99 <101084413+MSY99@users.noreply.github.com>
2022-11-16 03:00:29 +0900

    3am

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,14 @@
-<?
-    $connect=mysql_connect( "localhost", "root", "apmsetup", "DBname") or  
-        die( "Can't connect to DB server"); 
-
-    mysql_select_db("DBname",$connect);
+<?php
+    $connect = mysqli_connect( "localhost", "team07", "team07", "team07");
+    if(!$connect){
+        echo "Database Connection Error";
+    }
+    else{
+        // 커넥션 체크용 나중에 주석처리
+        echo "Database Connection Success";
+    }
+    if(mysqli_connect_errno()){
+        echo "Could not connect : ".mysqli_connect_error();
+        exit();
+    }
 ?>
---
+++ search.php
@@ -0,0 +1,18 @@
+<?php
+    include 'dbconn.php';
+    $key = $_GET['search_key'];
+
+    $title_sql = 'SELECT originalTitle, posterPath FROM movie_metadata WHERE originalTitle LIKE '$key';';
+    $actor_sql = 'SELECT cma.movieId, cma.posterPath, cma.characterName
+    from (SELECT  c.movieId, m.posterPath, c.characterName, a.actorName from characters c 
+    inner join movie_metadata m on c.movieId = m.movieId
+    inner join actor a on c.actorId = a.actorId) cma
+    where cma.actorName ='$key';';
+    $direct_sql = 'SELECT originalTitle, posterPath from movie_metadata 
+    where directorId = (SELECT directorId FROM director WHERE directorName = '$key');';
+    $key_sql = 'SELECT originalTitle, posterPath from movie_metadata
+    where movieId in (select movieId from describes 
+    where keywordId = (select keywordId from keyword where keywordName = '$key')) ;';
+
+
+?>
