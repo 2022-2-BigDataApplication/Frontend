2481ad41895f51bed8866aa4cab1621a9e74afa1
uommou <ffffff.990917@gmail.com>
2022-11-17 06:37:05 +0900

    table로 바꿈

--- p_exploreIndex.php
+++ p_exploreIndex.php
@@ -33,43 +33,81 @@ include('dbconn.php');
             <h2>Filtering Option</h2>
             <br>
         <div class="filtering">
-            RelesedDate:<input type="text" name="start_year" placeholder="start_year" value='1920'>
-                        <input type="text" name="end_year" placeholder="end_year" value='2022'>
+        <table>
+            <tr>
+                <td>RelesedDate : </td>
+                <td><input type="text" name="start_year" placeholder="start_year" value="1920">
+                <input type="text" name="end_year" placeholder="end_year" value="2022">
+                </td>
+            </tr>    
                         <br><br>
                             
-            Genre : <input type="checkbox" name="genre[]" value="21" checked>All
+            <tr>
+                <td>Genre : </td>
+                <td>
+                    <input type="checkbox" name="genre[]" value="21" checked>All
                     <input type="checkbox" name="genre[]" value="16">Animation
                     <input type="checkbox" name="genre[]" value="12">Adventure
                     <input type="checkbox" name="genre[]" value="10749">Romance
                     <input type="checkbox" name="genre[]" value="35">Comedy
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
                     <input type="checkbox" name="genre[]" value="28">Action
                     <input type="checkbox" name="genre[]" value="10751">Family
                     <input type="checkbox" name="genre[]" value="36">History
                     <input type="checkbox" name="genre[]" value="18">Drama
                     <input type="checkbox" name="genre[]" value="80">Crime
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>   
                     <input type="checkbox" name="genre[]" value="878">Science Fiction
                     <input type="checkbox" name="genre[]" value="14">Fantasy
                     <input type="checkbox" name="genre[]" value="10402">Music
                     <input type="checkbox" name="genre[]" value="27">Horror
                     <input type="checkbox" name="genre[]" value="99">Documentary
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td> 
                     <input type="checkbox" name="genre[]" value="9648">Mystery
                     <input type="checkbox" name="genre[]" value="53">Thriller
                     <input type="checkbox" name="genre[]" value="37">Western
                     <input type="checkbox" name="genre[]" value="10770">TV Movie
                     <input type="checkbox" name="genre[]" value="10752">War
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td> 
                     <input type="checkbox" name="genre[]" value="10769">Foreign
+                </td>
+            </tr>
                     <br><br>
 
-            Age Limitation:<input type="radio" name="age" value="all" checked>All
-                            <input type="radio" name="age" value="kids">Kids
-                    <br><br>
-
-            Viewing : <select name="view">
-
-                <option value="0">Recently Released</option>
-                <option value="1">Most Commented</option>
-                <option value="2">Highly Scored</option>
-            </select>   
+            <tr>
+                <td>Age Limitation : </td>
+                <td><input type="radio" name="age" value="all" checked>All
+                    <input type="radio" name="age" value="kids">Kids
+                </td> 
+            </tr>
+                <br><br>
+            <tr>
+                <td>Viewing : </td>
+                <td>
+                    <select name="view">
+                    <option value="0" default>Recently Released</option>
+                    <option value="1">Most Commented</option>
+                    <option value="2">Highly Scored</option>
+                    </select>
+                </td>
+            </tr>
+        </table>  
                     <br><br><br>
 
             <button>SUBMIT</button>
--- p_exploreResult.php
+++ p_exploreResult.php
@@ -38,46 +38,86 @@ $view= $_POST['view'];
         <form action="p_exploreResult.php" method="post">
             <h2>Filtering Option</h2>
             <br>
-            
-            RelesedDate:<input type="text" name="start_year" placeholder="start_year" value="<?php echo $start_year; ?>">
-                        <input type="text" name="end_year" placeholder="end_year" value="<?php echo $end_year; ?>">
+        <div class="filtering">    
+        <table>
+            <tr>
+                <td>RelesedDate : </td>
+                <td><input type="text" name="start_year" placeholder="start_year" value="<?php echo $start_year; ?>">
+                <input type="text" name="end_year" placeholder="end_year" value="<?php echo $end_year; ?>">
+                </td>
+            </tr>    
                         <br><br>
                             
-                Genre : <input type="checkbox" name="genre[]" value="21" checked>All
+            <tr>
+                <td>Genre : </td>
+                <td>
+                    <input type="checkbox" name="genre[]" value="21" checked>All
                     <input type="checkbox" name="genre[]" value="16">Animation
                     <input type="checkbox" name="genre[]" value="12">Adventure
                     <input type="checkbox" name="genre[]" value="10749">Romance
                     <input type="checkbox" name="genre[]" value="35">Comedy
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
                     <input type="checkbox" name="genre[]" value="28">Action
                     <input type="checkbox" name="genre[]" value="10751">Family
                     <input type="checkbox" name="genre[]" value="36">History
                     <input type="checkbox" name="genre[]" value="18">Drama
                     <input type="checkbox" name="genre[]" value="80">Crime
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>   
                     <input type="checkbox" name="genre[]" value="878">Science Fiction
                     <input type="checkbox" name="genre[]" value="14">Fantasy
                     <input type="checkbox" name="genre[]" value="10402">Music
                     <input type="checkbox" name="genre[]" value="27">Horror
                     <input type="checkbox" name="genre[]" value="99">Documentary
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td> 
                     <input type="checkbox" name="genre[]" value="9648">Mystery
                     <input type="checkbox" name="genre[]" value="53">Thriller
                     <input type="checkbox" name="genre[]" value="37">Western
                     <input type="checkbox" name="genre[]" value="10770">TV Movie
                     <input type="checkbox" name="genre[]" value="10752">War
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td> 
                     <input type="checkbox" name="genre[]" value="10769">Foreign
+                </td>
+            </tr>
                     <br><br>
 
-                Age Limitation: <input type="radio" name="age" value="all" checked>All
-                            <input type="radio" name="age" value="kids">Kids
-                    <br><br>
-
-            Viewing : <select name="view">
-                <option value="0" default>Recently Released</option>
-                <option value="1">Most Commented</option>
-                <option value="2">Highly Scored</option>
-            </select>   
+            <tr>
+                <td>Age Limitation : </td>
+                <td><input type="radio" name="age" value="all" checked>All
+                    <input type="radio" name="age" value="kids">Kids
+                </td> 
+            </tr>
+                <br><br>
+            <tr>
+                <td>Viewing : </td>
+                <td>
+                    <select name="view">
+                    <option value="0" default>Recently Released</option>
+                    <option value="1">Most Commented</option>
+                    <option value="2">Highly Scored</option>
+                    </select>
+                </td>
+            </tr>
+        </table>     
             <br><br><br>
             <center><button>SUBMIT</button></center>
             <br><br><br>
+        </div>
         </form>
     </div>
     <div class="result">
